49fbfea4935b7fe4c57bab9d7102809bff881e12
Olimjon <tajiev0711@gmail.com>
2023-01-20 17:12:02 +0500

    Create Villa DTOs

---
+++ WebVilla/Models/DTOs/CreateVillaDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebVilla.Models.DTOs
+{
+    public class CreateVillaDto
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public double Rate { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public IFormFile ImageUrl { get; set; }
+        public string Amenity { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ WebVilla/Models/DTOs/UpdateVillaDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebVilla.Models.DTOs
+{
+    public class UpdateVillaDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public double Rate { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public IFormFile ImageUrl { get; set; }
+        public string Amenity { get; set; }
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
